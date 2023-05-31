023f4bb3c294812b77fc67fcdc268ecac94dc876
naosmile <naosmile0731@gmail.com>
2023-05-31 10:02:34 +0900

    Basic認証

--- db/schema.rb
+++ db/schema.rb
@@ -13,9 +13,12 @@
 ActiveRecord::Schema.define(version: 2023_05_30_063845) do
 
   create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "name"
-    t.string "text"
-    t.text "image"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "content"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
