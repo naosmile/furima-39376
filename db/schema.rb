f2cb73d51a1ff79c460af10e0452d5028550dc5d
naosmile <naosmile0731@gmail.com>
2023-06-03 16:36:45 +0900

    usersテーブル作成

--- db/schema.rb
+++ db/schema.rb
@@ -17,12 +17,6 @@ ActiveRecord::Schema.define(version: 2023_06_03_001559) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.text "content"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
-
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
     t.string "email", default: "", null: false
