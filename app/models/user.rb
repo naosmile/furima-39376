91784de28e4d1d11b7b9da1fe5d883fc7dbf5381
naosmile <naosmile0731@gmail.com>
2023-06-05 15:19:11 +0900

    バリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname,                   presence: true
+  validates :last_name_full_width,       presence: true       
+  validates :first_name_full_width,      presence: true   
+  validates :last_name_kana_full_width,  presence: true  
+  validates :first_name_kana_full_width, presence: true   
+  validates :date_of_birth,              presence: true  
+
 end
